a028f674d24d56b64cfe97f3c58d1e0ad1494486
Puneet-Amrutkar <puneet.tech15@gmail.com>
2023-04-20 02:02:03 +0530

    useless change

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -6,11 +6,11 @@ int main()
 {
     cout << "Enter the elements of the array";
     int arr[10];
-    for (int i = 0; i<10 ; i++)
+    for (int i = 0 ; i<10 ; i++)
     {
         cin >> arr[i];
     }
-    for (int i = 0; i<10 ; i++)
+    for (int i = 0 ; i<10 ; i++)
     {
         cout << arr[i];
     }
